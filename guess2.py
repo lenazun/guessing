fc194f7c04954497e9382c840258b7595fb43b7b
Hackbright Student <info@hackbrightacademy.com>
2014-09-30 15:15:35 -0700

    Initial commit of functioning guessing game files to repository. Guess doesn't have string check. Guess2 does.

---
+++ guess2.py
@@ -0,0 +1,27 @@
+from random import randint 
+
+name = raw_input("Howdy, what's your name? > ")
+
+print("Hi %s, I'm thinking of a number between 1 and 100. Try to guess my number." % name)
+
+number = randint(1, 100)
+
+game = True
+
+guessCount = 0
+
+while game:
+    try:
+        guess = int(raw_input("Your guess? > "))
+
+        guessCount += 1
+        if guess > number:
+            print("Your guess is too high, try again")
+        elif guess < number:
+            print("Your guess is too low, try again")
+        else:
+            game = False
+            print "Good job! You found my guess in %i tries" % guessCount
+    except ValueError:
+        print "Oops!  You didn't enter a valid number."
+
