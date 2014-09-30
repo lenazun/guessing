c62cb753f487267243b406905af16cc83009ba62
Hackbright Student <info@hackbrightacademy.com>
2014-09-30 15:57:32 -0700

    Made safenumber function. Have error where 'out of range' prints with bad input.

--- guess2.py
+++ guess2.py
@@ -1,9 +1,15 @@
 from random import randint 
 
+def safenumber(x):
+    try:
+        x = int(x)
+        return x
+    except ValueError:
+        print "Ooops that's not a valid number"    
+
+
 def guessing():
     print("I'm thinking of a number between 1 and 100. Try to guess my number.")
-
-
 
     number = randint(1, 100)
 
@@ -12,22 +18,20 @@ def guessing():
     guessCount = 0
 
     while game:
-        try:
-            guess = int(raw_input("Your guess? > "))
-            if guess < 1 or guess > 100:
-                print "That's out of range silly"
+        
+        guess = safenumber(raw_input("Your guess? > "))
+        if guess < 1 or guess > 100:
+            print "That's out of range silly"
+        else:
+            guessCount += 1
+            if guess > number:
+                print("Your guess is too high, try again")
+            elif guess < number:
+                print("Your guess is too low, try again")
             else:
-                guessCount += 1
-                if guess > number:
-                    print("Your guess is too high, try again")
-                elif guess < number:
-                    print("Your guess is too low, try again")
-                else:
-                    game = False
-                    print "Good job! You found my guess in %i tries." % guessCount
-        except ValueError:
-            print "Oops!  You didn't enter a valid number."
-
+                game = False
+                print "Good job! You found my guess in %i tries." % guessCount
+        
 def main():
     name = raw_input("Howdy, what's your name? > ")
     print("Hi %s!" % name)
@@ -44,4 +48,11 @@ def main():
 
 main()
 
-   
+  
+# g = ri() 
+# try:
+#   x = int(g)
+# except ValueError:
+#   kjkllkjlkj
+
+# hfghgfhgfhfg
